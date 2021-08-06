5472b99a6948ab724d82c277943cbd65f9cfd250
geekrst <geekrst@gmail.com>
2021-08-06 09:09:18 +0530

    Initial commit

---
+++ src/Hello.java
@@ -0,0 +1,17 @@
+public class Hello {
+    public static void main(String []args){
+        System.out.println("Hello Rajesh Singh");
+        System.out.println("How R u??");
+
+        int myValue=10000;
+
+        int minValue= Integer.MIN_VALUE;
+        int maxValue=Integer.MAX_VALUE;
+
+        System.out.println("Minimum value is :"+minValue);
+        System.out.println("Maximum value is :"+maxValue);
+
+        System.out.println("My Value is :"+myValue);
+
+    }
+}
