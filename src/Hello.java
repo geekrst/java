ef7c1f43eb6ea5bcb765fb419a46179cbf9669a9
geekrst <geekrst@gmail.com>
2021-08-10 09:24:14 +0530

    tested values of BYTE and SHORT

--- src/Hello.java
+++ src/Hello.java
@@ -13,5 +13,26 @@ public class Hello {
 
         System.out.println("My Value is :"+myValue);
 
+        int maxInt = Integer.MAX_VALUE +1;
+        System.out.println("maxInt is :"+maxInt);
+
+        int maxIntPlusOne = 2_147_483_647;
+
+        System.out.println(maxIntPlusOne);
+
+        byte minByte= Byte.MIN_VALUE;
+        byte maxByte=Byte.MAX_VALUE;
+
+        System.out.println("Minimum BYTE value is :"+minByte);
+        System.out.println("Maximum BYTE value is :"+maxByte);
+
+        short minShort= Short.MIN_VALUE;
+        short maxShort=Short.MAX_VALUE;
+
+        System.out.println("Minimum Short value is :"+minShort);
+        System.out.println("Maximum Short value is :"+maxShort);
+
+
+
     }
 }
