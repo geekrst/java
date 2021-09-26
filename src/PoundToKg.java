36ecd6d3794ed4c49b69ac250f0170c941b6182b
geekrst <geekrst@gmail.com>
2021-09-26 08:43:06 +0530

    corrected double value

--- src/PoundToKg.java
+++ src/PoundToKg.java
@@ -5,7 +5,7 @@ public class PoundToKg {
     public static void main(String [] args){
         Scanner sc=new Scanner(System.in);
         float pound=sc.nextFloat();
-        double kg = (pound * 0.45359237d);
+        double kg = (pound * 0.45359237);
         System.out.println(kg);
     }
 }
